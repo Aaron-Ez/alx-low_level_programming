612ac9c80ddb8d2d75a14d587c8036b0d8a89ccf
Aaron-Ez <stcyril20@gmail.com>
2022-06-02 11:35:38 +0000

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main prints Programming is like building a multilingual puzzle
+ *return: 0 print if its exited properly
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
