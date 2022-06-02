e954faddb51ce22752a7ffd742f1569f2ac26848
Aaron-Ez <stcyril20@gmail.com>
2022-06-02 13:17:34 +0000

    last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints and the piece of art is useful"
+ * -Dora Korpar, 2015-10-19
+ *  Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
