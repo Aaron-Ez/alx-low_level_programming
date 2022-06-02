7f86e75eb1feeefa97129c76b293e4fd0889175b
Aaron-Ez <stcyril20@gmail.com>
2022-06-02 12:25:20 +0000

    modify

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints with proper grammar,
- *but the outcome is a piece of art
+ * but the outcome is a piece of art
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
