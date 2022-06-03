7478e7a370dffc33f011451d033ba96f1437338d
Aaron-Ez <stcyril20@gmail.com>
2022-06-03 22:33:00 +0000

    last commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print 2 digit numbers in a pair 2 times
+ * ignore repeated sequence b/n two paires
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (x = i; x < 58; x++)
+			{
+				for (y = j; y < 50; y++)
+				{
+					if (x == i && y == j)
+						continue;
+					else
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(x);
+						putchar(y);
+						if (i < 57 || j < 56 || x < 57 || y < 57)
+						{
+							putchar(44);
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
