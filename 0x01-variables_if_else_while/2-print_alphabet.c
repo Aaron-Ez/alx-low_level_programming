2bf006462a9a86af67696d4ee53435da149a43c1
Aaron-Ez <stcyril20@gmail.com>
2022-06-03 12:48:25 +0000

    print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Description: print alphabets a-z
+ * by using putchar() function
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
