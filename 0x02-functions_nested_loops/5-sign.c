2545ab5ffa332bc5e4d20914e69e7ef7fb0c25c0
Aaron-Ez <stcyril20@gmail.com>
2022-06-07 21:30:25 +0000

    checks for sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,11 +12,20 @@ int print_sign(int n)
 	int x;
 
 	if (n > 0)
+	{
 		x = 1;
+		_putchar('+');
+	}
 	else if (n == 0)
+	{
 		x = 0;
+		_putchar('0');
+	}
 	else if (n < 0)
+	{
 		x = -1;
+		_putchar('-');
+	}
 
 	return (x);
 }
