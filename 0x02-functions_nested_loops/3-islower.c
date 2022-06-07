c856769b42d04e0c5de47417e6ff74cf323fda64
Aaron-Ez <stcyril20@gmail.com>
2022-06-07 20:53:25 +0000

    check uppercase or smallcase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,19 @@
 
 /**
  * _islower checks if its lowercase or uppercase
- * Return: 1 if the given letter is lowercase 
+ * c takes letter and checks if lower or upper case
+ * Return: 1 if the given letter is lowercase
  * 0 if the given letter is uppercase
  */
 
 int _islower(int c)
 {
 	int x;
- 	if (c >= 97 && c <= 122)
-		x=1;
-	else
-		x=0;
 
-	return x;
+ 	if (c >= 97 && c <= 122)
+		x = 1;
+	else
+		x = 0;
+
+	return (x);
 }
