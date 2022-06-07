bcb5884339208b3a7f140db7fe280df5978b0e4d
Aaron-Ez <stcyril20@gmail.com>
2022-06-07 21:32:38 +0000

    checks for sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 
 /**
  * print_sign - checks for if its positive
- * or negative or zero 
- * @n - takes input and checks for it
- * Return: 1 if its positive 0 if its zero 
+ * or negative or zero
+ * n - takes input and checks for it
+ * Return: 1 if its positive 0 if its zero
  * -1 if its negative
  */
 int print_sign(int n)
