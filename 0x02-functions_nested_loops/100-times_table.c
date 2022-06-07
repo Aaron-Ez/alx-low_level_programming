5a27e0195429ef69f77cb7e8b6c0e975ac94e4c5
Aaron-Ez <stcyril20@gmail.com>
2022-06-07 23:06:08 +0000

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,28 +16,25 @@ void print_times_table(int n)
 		{
 			for (y = 0; y <= n; y++)
 			{
-				z = y & x;
+				z = y * x;
 				if (y == 0)
 				{
 					_putchar(z + '0');
-				}
-				else if (z < 10 && y != 0)
+				}else if (z < 10 && y != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(z + '0');
-				}
-				else if (z >= 10 && z < 100)
+				}else if (z >= 10 && z < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-				else if (z >= 100)
+				}else if (z >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
