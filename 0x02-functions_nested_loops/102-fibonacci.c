517449553e34ae1b47c04d69956863fbc521fb8f
Aaron-Ez <stcyril20@gmail.com>
2022-06-07 23:20:53 +0000

    the first 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, j = 1, k = 2, sum = 0;
+	
+	for (i = 0; i < 49; i++)
+	{
+		printf("%ld, ", j);
+		sum = j + k;
+		j = k;
+		k = sum;
+		if (i == 48)
+			printf("%id\n", x);
+	}
+}
