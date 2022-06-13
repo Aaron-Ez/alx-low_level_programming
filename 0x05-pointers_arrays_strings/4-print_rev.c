40fb8dd27f503b939bdbd183d3f1e8f52e98a2c5
Aaron-Ez <stcyril20@gmail.com>
2022-06-13 12:04:20 +0000

    print rev order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ * s - accepts string argument
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int x = 0;
+
+	for (i = 0; s[i] > 0; i++)
+	{
+		x++;
+	}
+
+	for (i = x-1; s[i] > 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
