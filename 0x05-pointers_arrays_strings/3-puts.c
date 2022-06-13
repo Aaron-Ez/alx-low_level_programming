fbcca06eaf9f06a9820a4aa637b3ce0945b6d871
Aaron-Ez <stcyril20@gmail.com>
2022-06-13 10:32:45 +0000

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - takes arument and prints the arument
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void reset_t0_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
