d9e1e5965779e27ff471a749fa1914a39e4091dd
Aaron-Ez <stcyril20@gmail.com>
2022-06-13 12:37:32 +0000

    swap two numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,15 @@
 
 /**
  * swap_int - swaps two integers value
- * a and b paremets takes two argument
+ * @a: the first num
+ * @b: the second num
  * Return: void
  */
 
 void swap_int(int *a, int *b)
 {
 	int x;
-	
+
 	x = *a;
 	*a = *b;
 	*b = x;
