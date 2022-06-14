594ce262134203a69d303ec1d51707e325e1c199
Aaron-Ez <stcyril20@gmail.com>
2022-06-14 10:18:38 +0000

    crack password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,11 +10,20 @@
 
 int main(void)
 {
-	int num;
+	int r = 0;
+	int c = 0;
+	time_t t;
 
-	num = rand();
-	srand(time(0));
-	printf("%i\n", num);
+	srand((unsigned int) time(&t));
 
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
 	return (0);
 }
