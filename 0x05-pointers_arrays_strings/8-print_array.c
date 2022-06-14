3fd056ef7e89736977a1f123a51b308fe6639af5
Aaron-Ez <stcyril20@gmail.com>
2022-06-14 08:13:38 +0000

    print array content

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints the content of array as give range
+ * @a: accpets array name 
+ * @n: accpets the range of the array to prints its content
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i <= n - 2)
+		{
+			printf(", ");
+		}
+	}
+}
