1eec2885aa05c071413387968e086e92fe3021e4
Aaron-Ez <stcyril20@gmail.com>
2022-06-14 08:22:58 +0000

    print array content

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,11 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i <= n - 1)
+		if (i < n - 1)
 		{
 			printf(", ");
 		}
 	}
-	_putchar('\n');
+	
+	printf("\n");
 }
