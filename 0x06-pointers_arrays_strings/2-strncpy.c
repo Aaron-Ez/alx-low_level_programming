2ec42e9bfef256578316be45cdec84bbd6ee1fa3
Aaron-Ez <stcyril20@gmail.com>
2022-06-15 19:17:00 +0000

    copy string with range

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string from src to dest
+ * @dest: the place where the source copies to
+ * @src: the string which is copied
+ * Return: dest string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
