d7ac184b1ad3cb59452e43491fd770c9ef96632e
Aaron-Ez <stcyril20@gmail.com>
2022-06-16 00:44:34 +0000

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @s: accepts a string
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char a[] = "ol_ea__t";
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+
+	return (s);
+}
