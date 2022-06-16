b9a87d296ffad7eb4c8bf71b7391c08c711e9f09
Aaron-Ez <stcyril20@gmail.com>
2022-06-16 00:15:16 +0000

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,20 @@
 char *cap_string(char *s)
 {
 	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'A' && s[i] <= 'Z')
 		{
 			s[i] = s[i] + 32;
+			for (j = 0; delimit[j] != '\0'; j++)
+			{
+				if (s[i] == delimit[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
 		}
 	}
 
