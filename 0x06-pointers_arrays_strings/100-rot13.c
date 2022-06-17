0b32a408e09483c684b25393c19f39ceba8ccf07
Aaron-Ez <stcyril20@gmail.com>
2022-06-17 09:25:12 +0000

    cipher rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - add 13 to a string single char and replace with it
+ * @s: accpets a string argument
+ * Return: rotated string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	
+	char string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char stringrot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[i] == string[j])
+			{
+				s[i] = stringrot[j];
+			}
+		}
+	}
+
+	return (s);
+}
