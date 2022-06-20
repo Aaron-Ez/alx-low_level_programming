47cfb80fa4f843484c867aa54e55b4576a2a5b0a
Aaron-Ez <stcyril20@gmail.com>
2022-06-20 19:16:27 +0000

    print the chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchar(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char * haystack, char *needle);
+void print_chessboard(char (*a)[8]);
  
