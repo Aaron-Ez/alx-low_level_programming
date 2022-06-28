b1c116457c4c1888e80143aa332beb13f7614af3
Aaron-Ez <stcyril20@gmail.com>
2022-06-28 20:25:47 +0000

    first commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	return (0);
+		return (0);
+
 	p = malloc((size) * sizeof(char));
+
 	if (p == '\0')
-	return (0);
+		return (0);
+
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
