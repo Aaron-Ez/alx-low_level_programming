72c394d6fe2af7fdc4ff2a6d4be8471ff54cc99c
Aaron-Ez <stcyril20@gmail.com>
2022-07-05 00:48:30 +0000

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifdef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: this struct for dog info
+ */
+
 struct dog {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
 #endif
