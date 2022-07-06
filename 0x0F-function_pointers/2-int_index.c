3a1cd2ca67370d217a3b74315afbe1b10aa6604e
Aaron-Ez <stcyril20@gmail.com>
2022-07-06 22:37:58 +0000

    search for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search integer
+ * @array: holds elements
+ * @size: no of elements
+ * @cmp: compare values
+ * Return: the first element
+ * no match return -1
+ * size <= 0 return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && cmp)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (cmp(array[i] != 0))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
